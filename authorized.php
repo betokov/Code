20b50759a00af02ba07b8956fc0ef836c41c1952
unknown <anzarsh@mail.ru>
2016-12-09 14:20:10 +0300

    authorized

--- authorized.php
+++ authorized.php
@@ -26,6 +26,14 @@ $error = '';
 	</style>
 </head>
 <body>
+	<?php 
+	if(!isset($_SESSION ['user_id'])){
+		if (isset($_COOKIE['user_id']) && isset($_COOKIE['username'])){
+			$_SESSION['user_id'] = $_COOKIE['user_id';]
+			$_SESSION['username'] = $_COOKIE['username';]
+		}
+	}
+	?>
 	<?php
 	if(!isset($_SESSION['id'])){
 		if(isset($_POST['submit'])){
