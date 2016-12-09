01aa80f7f27e1532de751f65cddb20a9ff322d62
Barasbi <betokov93@gmail.com>
2016-12-09 20:25:03 +0300

    change calc

--- calc.php
+++ calc.php
@@ -14,34 +14,61 @@
 			text-align:left;
 			margin-left:60px;
 		}
+		h2{
+			text-align:center;
+		}
 	</style>
 </head>
 <body>
-<?php
-if(!isset($_POST['submit'])){
-	$a = $_POST['n_1'];
-	$b = $_POST['n_2'];
-	$action = $_POST['action'];
+	<div>
+		<h1>Калькулятор</h1>
+		<?php
+		if(!isset($_POST['submit'])){
+			$a = $_POST['n_1'];
+			$b = $_POST['n_2'];
+			$action = $_POST['action'];
 
-	switch($action){
-		case mull:
-			mull($a, $b);
-			break;
-	}
+			switch($action){
+				case sum:
+				sum($a, $b);
+				break;
+				
+				case sub:
+				sub($a, $b);
+				break;
 
-}
-?>
-<div>
-	<h1>Калькулятор</h1>
-		<?php 	?>
-	<form action="calc.php" method="post">
-		<input type="text" name="n_1">
-		<select name="action">
-			<option value="mull">+</option>
-		</select>
-		<input type="text" name="n_2"><br><br>
-		<input type="submit">
-	</form>
-</div>
+				case mul:
+				mul($a, $b);
+				break;
+
+				case del:
+				del($a, $b);
+				break;
+
+				case sqr:
+				sqr($a, $b);
+				break;
+
+				case kvad:
+				kvad($a, $b);
+				break;
+			}
+
+		}
+		?>
+		<form action="calc.php" method="post">
+			<input type="text" name="n_1" value="<?php if(!empty($a)) echo $a; ?>">
+			<select name="action">
+				<option value="sum" <?php if($action == 'sum') echo 'selected';?>>+</option>
+				<option value="sub" <?php if($action == 'sub') echo 'selected';?>>-</option>
+				<option value="mul" <?php if($action == 'mul') echo 'selected';?>>*</option>
+				<option value="del" <?php if($action == 'del') echo 'selected';?>>:</option>
+				<option value="sqr" <?php if($action == 'sqrt') echo 'selected';?>>&#8730;</option>
+				<option value="kvad" <?php if($action == 'kvad') echo 'selected';?>>в степени</option>
+			</select>
+			<input type="text" name="n_2" value="<?php if(!empty($b)) echo $b; ?>"><br><br>
+			<input type="submit">
+		</form>
+	</div>
 </body>
 </html>
