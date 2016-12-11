cbbb8241efa669d8204a06f913e8b33e01aa3e9b
Barasbi <web_masters_07@mail.ru>
2016-12-11 14:51:02 +0300

    nothing

--- authorized.php
+++ authorized.php
@@ -8,30 +8,36 @@
 <body>
 	<?php 
 
-	$dbc = mysqli_connect('localhost', 'root', '') or die('ошибка подключения к базе данных');
-	mysqli_select_db($dbc, 'code-php') or die ('ошибка выбора базы данных');
-		$query = "SELECT 'login', 'password' FROM authorized WHERE password = SHA('password')";
-		$result = mysqli_query($dbc, $query) or die('Ошибка запроса');
-		while($row = mysqli_fetch_array($result)){
-			$login = $row['login'];
-			$password = $row[SHA('password')];
-		}
-	mysqli_close($dbc);
-	
-	if(!isset($_SERVER['PHP_AUTH_USER']) || 
-		!isset($_SERVER['PHP_AUTH_PW']) || 
-		($_SERVER['PHP_AUTH_USER'] != $login) || 
-		($_SERVER['PHP_AUTH_PW'] != $password)){
+
+
+	if(!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) || 
+		($_SERVER['PHP_AUTH_USER'] != $login) || ($_SERVER['PHP_AUTH_PW'] != $password)){
 	header('Content-Type: text/html');
 	header('HTTP/1.1 401 Unauthorized');
-	header('WWW-Authenticate:Basic realm="Authorized"');
-	exit('<h2>Извините, вы ввели неправильные логин или пароль</h2>');
+	header('WWW-Authenticate: Basic realm="Authorized"');
+	exit('<h2>Вы нажали отмена</h2>');
 } 
-else{
-	header('Content-Type:text/plain');
-	header('Location: http://www.google.ru');
-	header('Refresh: 5; url=http://www.google.ru');
-}
+	$dbc = mysqli_connect('localhost', 'root', '') or die('ошибка подключения к базе данных');
+	mysqli_select_db($dbc, 'code-php') or die ('ошибка выбора базы данных');
+	$login = mysqli_real_escape_string($dbc, trim($_SERVER['PHP_AUTH_USER']));
+	$password = mysqli_real_escape_string($dbc, trim($_SERVER['PHP_AUTH_PW']));
+
+	$query = "SELECT id, login FROM authorized WHERE login = '$login' AND
+	 password = SHA('$password')";
+	$result = mysqli_query($dbc, $query) or die('Ошибка запроса');
+	if(mysqli_num_rows($result) == 1){
+		$row = mysqli_fetch_array($result);
+		$id = $row['id'];
+		$login = $row['login'];
+	}
+	else{
+		header('HTTP/1.1 401 Unauthorized');
+	header('WWW-Authenticate: Basic realm="Authorized"');
+	exit('<h2>Вы ввели не правильный логин или пароль</h2>');
+	}
+	mysqli_close($dbc);
+echo "Вы вошли как $login";
+
 ?>
 </body>
 </html>
